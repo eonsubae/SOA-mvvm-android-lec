fd54ffa4a090fc8911540b44f5da50263d103b41
eonsubae <eonsubae@naver.com>
2020-10-13 17:03:58 +0900

    Navigation actions

--- app/src/main/java/com/example/animals/view/ListFragment.kt
+++ app/src/main/java/com/example/animals/view/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.animals.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -17,4 +19,11 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonDetail.setOnClickListener {
+            val action = ListFragmentDirections.actionDetail()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
