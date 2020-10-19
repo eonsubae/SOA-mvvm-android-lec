8f39b8fc9ad73db4235154082414acc7477df6b9
eonsubae <eonsubae@naver.com>
2020-10-19 12:48:08 +0900

    Inject the Shared Preferences

--- app/src/main/java/com/example/animals/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/animals/viewmodel/ListViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.animals.di.ApiComponent
+import com.example.animals.di.AppModule
 import com.example.animals.di.DaggerViewModelComponent
 import com.example.animals.model.Animal
 import com.example.animals.model.AnimalApiService
@@ -26,12 +27,16 @@ class ListViewModel(application: Application): AndroidViewModel(application) {
     @Inject
     lateinit var apiService: AnimalApiService
 
-    private val prefs = SharedPreferencesHelper(getApplication())
+    @Inject
+    lateinit var prefs: SharedPreferencesHelper
 
     private var invalidApiKey = false
 
     init {
-        DaggerViewModelComponent.create().inject(this)
+        DaggerViewModelComponent.builder()
+            .appModule(AppModule(getApplication()))
+                .build()
+                .inject(this)
     }
 
     fun refresh() {
