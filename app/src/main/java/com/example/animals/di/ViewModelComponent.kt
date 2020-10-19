e7518ce43b4bd8b7c15523a6b52f52ded645c866
eonsubae <eonsubae@naver.com>
2020-10-19 12:22:54 +0900

    Injecting the ApiService

---
+++ app/src/main/java/com/example/animals/di/ViewModelComponent.kt
@@ -0,0 +1,10 @@
+package com.example.animals.di
+
+import com.example.animals.viewmodel.ListViewModel
+import dagger.Component
+
+@Component(modules = [ApiModule::class])
+interface ViewModelComponent {
+
+    fun inject(viewModel: ListViewModel)
+}
